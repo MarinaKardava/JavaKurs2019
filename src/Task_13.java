5eb75ff4431e798b59407ab38fc3ce4e2b5b667b
MarinaKardava <MarKardava@yandex.ru>
2019-05-20 01:34:08 +0300

    Задача 13

---
+++ src/Task_13.java
@@ -0,0 +1,24 @@
+//Необходимо провести сравнение длинны двух строк, которые были введены с клавиатуры и записаны
+//
+// в соответствующие переменные. Вывести на экран строку с наибольшей длинной.
+
+import java.util.Scanner;
+
+public class Task_13 {
+    public static void main(String[] args) {
+
+        System.out.print("Введите строку 1: ");
+        Scanner in = new Scanner(System.in);
+        String str1 = in.nextLine();
+        System.out.print("Введите строку 2: ");
+        Scanner in1 = new Scanner(System.in);
+        String str2 = in1.nextLine();
+
+        int length1 = str1.length();
+        int length2 = str2.length();
+        System.out.println("Длинна 1-ой строки = "+length1 + "\nДлина 2-ой строки = "+length2);
+        if (length1 > length2) System.out.println(str1);
+        else System.out.println(str2);
+
+    }
+}
