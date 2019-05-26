9bd37fdcc452bff71a0b21a4f35ed1b7cfa28004
MarinaKardava <MarKardava@yandex.ru>
2019-05-26 15:39:21 +0300

    добавлено условие равенства двух строк

--- src/Task_13.java
+++ src/Task_13.java
@@ -11,13 +11,13 @@ public class Task_13 {
         Scanner in = new Scanner(System.in);
         String str1 = in.nextLine();
         System.out.print("Введите строку 2: ");
-        Scanner in1 = new Scanner(System.in);
-        String str2 = in1.nextLine();
+        String str2 = in.nextLine();
 
         int length1 = str1.length();
         int length2 = str2.length();
         System.out.println("Длинна 1-ой строки = "+length1 + "\nДлина 2-ой строки = "+length2);
         if (length1 > length2) System.out.println(str1);
+        else if (length1 == length2) System.out.println(str1 +"\n" + str2);
         else System.out.println(str2);
 
     }
